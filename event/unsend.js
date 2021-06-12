444abd509be4cacf8aa4c6d23904b77311b3d574
shinbunbun <takamuneyuto@gmail.com>
2021-06-12 05:19:34 +0000

    add unsend.js

--- event/unsend.js
+++ event/unsend.js
@@ -1,7 +1,7 @@
 exports.index = async (event) => {
   const message = {
     type: 'text',
-    text: 'ここは個チャだよ！',
+    text: `メッセージが取り消されました！\n取り消されたmessageId: ${event.unsend.messageId}`,
   };
   return message;
 };
