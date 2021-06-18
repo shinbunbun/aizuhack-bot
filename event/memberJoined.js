13fa90fc0467c3f3525fea85dcd48e1d2051d108
shinbunbun <takamuneyuto@gmail.com>
2021-06-18 19:36:39 +0000

    add comment

--- event/memberJoined.js
+++ event/memberJoined.js
@@ -1,7 +1,10 @@
+// メンバー参加イベントが飛んできた時
 exports.index = (event) => {
+  // 返信するメッセージを作成
   const message = {
     type: 'text',
     text: `ユーザーが参加しました！\n参加したユーザー: ${event.joined.members[0].userId}`,
   };
+  // 返信するメッセージをこの関数の呼び出し元（bot.js）に返す
   return message;
 };
--- event/memberLeft.js
+++ event/memberLeft.js
@@ -1,7 +1,10 @@
+// メンバー退出イベントが飛んできた時
 exports.index = (event) => {
+  // 返信するメッセージを作成
   const message = {
     type: 'text',
     text: `ユーザーが退出しました...\n退出したユーザー: ${event.left.members[0].userId}`,
   };
+  // 返信するメッセージをこの関数の呼び出し元（bot.js）に返す
   return message;
 };
