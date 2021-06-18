ba0c20db1311a0b407903d97bcb5be874d915ef9
shinbunbun <takamuneyuto@gmail.com>
2021-06-18 19:57:43 +0000

    add comment at message

--- event/message.js
+++ event/message.js
@@ -1,15 +1,20 @@
 // テキストメッセージの処理をする関数
 const textEvent = (event) => {
   let message;
+  // メッセージのテキストごとに条件分岐
   switch (event.message.text) {
+    // 'こんにちは'というメッセージが送られてきた時
     case 'こんにちは': {
+      // 返信するメッセージを作成
       message = {
         type: 'text',
         text: 'Hello, world',
       };
       break;
     }
+    // '複数メッセージ'というメッセージが送られてきた時
     case '複数メッセージ': {
+      // 返信するメッセージを作成
       message = [
         {
           type: 'text',
@@ -22,7 +27,9 @@ const textEvent = (event) => {
       ];
       break;
     }
+    // 'クイックリプライ'というメッセージが送られてきた時
     case 'クイックリプライ': {
+      // 返信するメッセージを作成
       message = {
         type: 'text',
         text: 'クイックリプライ（以下のアクションはクイックリプライ専用で、他のメッセージタイプでは使用できません）',
@@ -54,7 +61,9 @@ const textEvent = (event) => {
       };
       break;
     }
+    // 'スタンプメッセージ'というメッセージが送られてきた時
     case 'スタンプメッセージ': {
+      // 返信するメッセージを作成
       message = {
         type: 'sticker',
         packageId: '446',
@@ -62,7 +71,9 @@ const textEvent = (event) => {
       };
       break;
     }
+    // '画像メッセージ'というメッセージが送られてきた時
     case '画像メッセージ': {
+      // 返信するメッセージを作成
       message = {
         type: 'image',
         originalContentUrl: 'https://shinbunbun.info/images/photos/7.jpeg',
@@ -88,7 +99,9 @@ const textEvent = (event) => {
       };
       break;
     } */
+    // '位置情報メッセージ'というメッセージが送られてきた時
     case '位置情報メッセージ': {
+      // 返信するメッセージを作成
       message = {
         type: 'location',
         title: 'my location',
@@ -98,11 +111,14 @@ const textEvent = (event) => {
       };
       break;
     }
+    // 'イメージマップメッセージ'というメッセージが送られてきた時
     case 'イメージマップメッセージ': {
       // todo
       break;
     }
+    // 'ボタンテンプレート'というメッセージが送られてきた時
     case 'ボタンテンプレート': {
+      // 返信するメッセージを作成
       message = {
         type: 'template',
         altText: 'ボタンテンプレート',
@@ -149,7 +165,9 @@ const textEvent = (event) => {
       };
       break;
     }
+    // '確認テンプレート'というメッセージが送られてきた時
     case '確認テンプレート': {
+      // 返信するメッセージを作成
       message = {
         type: 'template',
         altText: '確認テンプレート',
@@ -172,7 +190,9 @@ const textEvent = (event) => {
       };
       break;
     }
+    // 'カルーセルテンプレート'というメッセージが送られてきた時
     case 'カルーセルテンプレート': {
+      // 返信するメッセージを作成
       message = {
         type: 'template',
         altText: 'カルーセルテンプレート',
@@ -232,7 +252,9 @@ const textEvent = (event) => {
       };
       break;
     }
+    // '画像カルーセルテンプレート'というメッセージが送られてきた時
     case '画像カルーセルテンプレート': {
+      // 返信するメッセージを作成
       message = {
         type: 'template',
         altText: '画像カルーセルテンプレート',
@@ -268,7 +290,9 @@ const textEvent = (event) => {
       };
       break;
     }
+    // 'Flex Message'というメッセージが送られてきた時
     case 'Flex Message': {
+      // 返信するメッセージを作成
       message = {
         type: 'flex',
         altText: 'Flex Message',
@@ -292,13 +316,18 @@ const textEvent = (event) => {
       };
       break;
     }
+    // 'ここはどこ'というメッセージが送られてきた時
     case 'ここはどこ': {
+      // 送信元がユーザーとの個チャだった場合
       if (event.source.type === 'user') {
+        // 返信するメッセージを作成
         message = {
           type: 'text',
           text: 'ここは個チャだよ！',
         };
+        // 送信元がグループだった場合
       } else if (event.source.type === 'group') {
+        // 返信するメッセージを作成
         message = {
           type: 'text',
           text: 'ここはグループだよ！',
@@ -306,7 +335,9 @@ const textEvent = (event) => {
       }
       break;
     }
+    // 上で条件分岐した以外のメッセージが送られてきた時
     default: {
+      // 返信するメッセージを作成
       message = {
         type: 'text',
         text: `受け取ったメッセージ: ${event.message.text}\nそのメッセージの返信には対応してません...`,
@@ -319,54 +350,67 @@ const textEvent = (event) => {
 
 // イメージイベントを処理する関数
 const imageEvent = () => {
+  // 返信するメッセージを作成
   const message = {
     type: 'text',
     text: '画像を受け取りました！',
   };
+  // 関数の呼び出し元（index）に返信するメッセージを返す
   return message;
 };
 
 // ビデオイベントを処理する関数
 const videoEvent = () => {
+  // 返信するメッセージを作成
   const message = {
     type: 'text',
     text: 'ビデオを受け取りました！',
   };
+  // 関数の呼び出し元（index）に返信するメッセージを返す
   return message;
 };
 
 // オーディオイベントを処理する関数
 const audioEvent = () => {
+  // 返信するメッセージを作成
   const message = {
     type: 'text',
     text: 'オーディオを受け取りました！',
   };
+  // 関数の呼び出し元（index）に返信するメッセージを返す
   return message;
 };
 
 // ファイルイベントを処理する関数
 const fileEvent = () => {
+  // 返信するメッセージを作成
   const message = {
     type: 'text',
     text: 'ファイルを受け取りました！',
   };
+  // 関数の呼び出し元（index）に返信するメッセージを返す
   return message;
 };
 
 // 位置情報イベントを処理する関数
 const locationEvent = (event) => {
+  // 返信するメッセージを作成
   const message = {
     type: 'text',
     text: `受け取った住所: ${event.message.address}`,
   };
+  // 関数の呼び出し元（index）に返信するメッセージを返す
   return message;
 };
 
 // スタンプメッセージを処理する関数
 const stickerEvent = (event) => {
   let message;
+  // スタンプのIDごとに条件分岐
   switch (event.message.stickerId) {
+    // スタンプのIDが1988だった場合
     case '1988': {
+      // 返信するメッセージを作成
       message = {
         type: 'sticker',
         packageId: '446',
@@ -374,7 +418,9 @@ const stickerEvent = (event) => {
       };
       break;
     }
+    // それ以外のIDだった場合
     default: {
+      // 返信するメッセージを作成
       message = {
         type: 'text',
         text: `受け取ったstickerId: ${event.message.stickerId}\nそのスタンプの返信には対応してません...`,
@@ -382,41 +428,60 @@ const stickerEvent = (event) => {
       break;
     }
   }
+  // 関数の呼び出し元（index）に返信するメッセージを返す
   return message;
 };
 
+// メッセージイベントが飛んできた時に呼び出される
 exports.index = async (event) => {
   let message;
+  // メッセージタイプごとの条件分岐
   switch (event.message.type) {
     case 'text': {
+      // テキストの場合はtextEventを呼び出す
+      // 実行結果をmessageに格納する
       message = textEvent(event);
       break;
     }
     case 'image': {
+      // イメージの場合はimageEventを呼び出す
+      // 実行結果をmessageに格納する
       message = imageEvent();
       break;
     }
     case 'video': {
+      // ビデオの場合はvideoEventを呼び出す
+      // 実行結果をmessageに格納する
       message = videoEvent();
       break;
     }
     case 'audio': {
+      // オーディオの場合はaudioEventを呼び出す
+      // 実行結果をmessageに格納する
       message = audioEvent();
       break;
     }
     case 'file': {
+      // ファイルの場合はfileEventを呼び出す
+      // 実行結果をmessageに格納する
       message = fileEvent();
       break;
     }
     case 'location': {
+      // 位置情報の場合はlocationEventを呼び出す
+      // 実行結果をmessageに格納する
       message = locationEvent(event);
       break;
     }
     case 'sticker': {
+      // スタンプの場合はstickerEventを呼び出す
+      // 実行結果をmessageに格納する
       message = stickerEvent(event);
       break;
     }
+    // それ以外の場合
     default: {
+      // 返信するメッセージを作成
       message = {
         type: 'text',
         text: 'そのイベントには対応していません...',
@@ -424,5 +489,6 @@ exports.index = async (event) => {
       break;
     }
   }
+  // 関数の呼び出し元（bot.jsのindex）に返信するメッセージを返す
   return message;
 };
