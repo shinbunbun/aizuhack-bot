9f87761ae14faeff6d73fdeaa9562a74fb56c87e
shinbunbun <takamuneyuto@gmail.com>
2021-06-18 20:01:32 +0000

    add comment at unsend

--- event/unsend.js
+++ event/unsend.js
@@ -1,7 +1,10 @@
+// 送信取消イベントが飛んできた時
 exports.index = async (event) => {
+  // 返信するメッセージを作成
   const message = {
     type: 'text',
     text: `メッセージが取り消されました！\n取り消されたmessageId: ${event.unsend.messageId}`,
   };
+  // 呼び出し元の関数（bot.jsのindex）に返信するメッセージを返す
   return message;
 };
