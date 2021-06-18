d062af66c868ac6bd4e3186847b6b892cfa55abf
shinbunbun <takamuneyuto@gmail.com>
2021-06-18 19:19:34 +0000

    add comment at server.js

--- api/server.js
+++ api/server.js
@@ -1,12 +1,16 @@
+// モジュールの読み込み
 const express = require('express');
 const line = require('@line/bot-sdk');
 const bot = require('../bot');
+// configの読み込み
 const config = require('../config').index();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+// /にアクセスがあった時、Deploy succeededと返す
 app.get('/', (req, res) => { res.send('Deploy succeeded'); });
+// /webhookにアクセスがあったとき、bot.jsのindexを呼び出す
 app.post('/webhook', line.middleware(config), bot.index);
 
 app.listen(PORT);
