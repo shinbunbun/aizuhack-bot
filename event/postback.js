2a4ae9ee6e069bfc81b76c51b51f0e86116c1d5c
shinbunbun <takamuneyuto@gmail.com>
2021-06-18 20:00:18 +0000

    add comment at postback & unfollow

--- event/postback.js
+++ event/postback.js
@@ -1,16 +1,23 @@
+// ポストバックイベントが飛んできた時
 exports.index = async (event) => {
   let message;
+  // ポストバックデータをpostbackDataに格納
   const postbackData = event.postback.data;
+  // もしevent.postback.paramsが存在する場合
   if (event.postback.params) {
+    // 返信するメッセージを作成
     message = {
       type: 'text',
       text: `日時データを受け取りました！\ndata: ${postbackData}\ndatetime: ${event.postback.params.datetime}`,
     };
+  // 存在しない場合
   } else {
+    // 返信するメッセージを作成
     message = {
       type: 'text',
       text: `ポストバックデータを受け取りました！\ndata: ${postbackData}`,
     };
   }
+  // 関数の呼び出し元（bot.jsのindex）に返信するメッセージを返す
   return message;
 };
--- event/unfollow.js
+++ event/unfollow.js
@@ -1,3 +1,5 @@
+// フォロー解除イベントが飛んできた時
 exports.index = (event) => {
+  // ログを出力
   console.log(`unfollowされました...\nuserId: ${event.source.userId}`);
 };
