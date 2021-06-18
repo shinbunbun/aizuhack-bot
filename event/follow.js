4ec3be9dcc531837cb76a56e50b384d4ac474e31
shinbunbun <takamuneyuto@gmail.com>
2021-06-18 19:32:13 +0000

    add comment at follow

--- event/follow.js
+++ event/follow.js
@@ -1,8 +1,10 @@
+// フォローイベントがとんできた時
 exports.index = () => {
+  // 返信するメッセージを作成
   const message = {
     type: 'text',
     text: '友達追加ありがとうございます！',
   };
-  console.log(message);
+  // 返信するメッセージをこの関数の呼び出し元（bot.js）に返す
   return message;
 };
