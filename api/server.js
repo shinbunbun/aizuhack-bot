87829c01f90b889a5a8e7304d6a9aa3f9c0ad63a
shinbunbun <takamuneyuto@gmail.com>
2022-05-18 12:05:32 +0000

    import -> require

--- api/server.js
+++ api/server.js
@@ -1,10 +1,8 @@
 // モジュールの読み込み
-import dotenv from 'dotenv';
-import express from 'express';
-import { middleware } from '@line/bot-sdk';
-import { index } from '../bot';
-
-dotenv.config();
+require('dotenv').config();
+const express = require('express');
+const line = require('@line/bot-sdk');
+const bot = require('../bot');
 
 // configの読み込み
 const config = require('../config').index();
@@ -15,7 +13,7 @@ const app = express();
 // /にアクセスがあった時、Deploy succeededと返す
 app.get('/', (req, res) => { res.send('Deploy succeeded'); });
 // /webhookにアクセスがあったとき、bot.jsのindexを呼び出す
-app.post('/webhook', middleware(config), index);
+app.post('/webhook', line.middleware(config), bot.index);
 
 app.listen(PORT);
 console.log(`Server running at ${PORT}`);
